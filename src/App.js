cff307357f672314e9247f766efa24b3c2a63a0b
Freddie Ridell <freddie@codogo.io>
2017-06-13 11:17:49 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from './logo.svg';
+
+export default () => (
+	<div>
+		Welcome to the codogo create-react-app-thing
+		<img src={logo} className="App-logo" alt="logo" />
+	</div>
+)
