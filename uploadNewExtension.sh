57d0554d3255db1ca0346160b36d6195eae19bb4
Freddie Ridell <freddie@codogo.io>
2017-06-29 17:20:43 +0100

    fix(bbpl): added eslint, doesn't halt deploy if no current extension

--- uploadNewExtension.sh
+++ uploadNewExtension.sh
@@ -6,23 +6,16 @@ WRITE_KEY="${WRITE_KEY:-bar}"
 
 EXTENSION_NAME="${EXTENSION_NAME:-Amazon Product Search}"
 
-echo "geting previous version extension id"
-
+echo "geting previous version extension id..."
 PREVIOUS_EXTENSION_ID="$( curl --progress-bar "https://api.cosmicjs.com/v1/extensions-2?hide_metafields=true&read_key=foo" | jq -r ".bucket.extensions | map(select(.title == \"$EXTENSION_NAME\" )) | .[0].id"  )"
 
 if [ "$PREVIOUS_EXTENSION_ID" == "null" ] ; then
 	echo "There is no '$EXTENSION_NAME' extension on cosmic to delete"
-	exit 0
 else
 	echo "Found id for '$EXTENSION_NAME': $PREVIOUS_EXTENSION_ID"
+	echo "deleting old extension..."
+	curl --progress-bar -X DELETE "https://api.cosmicjs.com/v1/$BUCKET_SLUG/extensions/$PREVIOUS_EXTENSION_ID" -d "{\"write_key\":\"$WRITE_KEY\"}" -H "Content-type: application/json" > /dev/null
 fi
 
-DELETE_BODY="{\"write_key\":\"$WRITE_KEY\"}"
-
-echo "deleting old extension"
-
-curl --progress-bar -X DELETE "https://api.cosmicjs.com/v1/$BUCKET_SLUG/extensions/$PREVIOUS_EXTENSION_ID" -d "$DELETE_BODY" -H "Content-type: application/json" > /dev/null
-
-echo "upload new version of extension"
-
+echo "upload new version of extension..."
 curl --progress-bar --url "https://api.cosmicjs.com/v1/$BUCKET_SLUG/extensions" --header "content-type: multipart/form-data" --form "write_key=$WRITE_KEY" --form "zip=@./build.zip" > /dev/null
